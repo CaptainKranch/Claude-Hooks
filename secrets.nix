3a67090c4b86bbcb13e6eda823f4b620b3d1136a
Daniel García Medina <danielgm32410@gmail.com>
2025-07-14 19:46:36 -0500

    Remove secrets before adding submodule

--- secrets.nix
+++
@@ -1,7 +0,0 @@
-let
-  danielgm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKeMSnEvoZrlPC7LMnlIEeLTQ3QLpdeM6njeXhtqFYrM dgm";
-in
-{
-  "secrets/telegram-token.age".publicKeys = [ danielgm ];
-  "secrets/telegram-chat-id.age".publicKeys = [ danielgm ];
-}
